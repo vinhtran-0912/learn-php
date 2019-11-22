5d4dfedc292e82b9744188eddb88cf16f2ea03ef
vinhtn-1790 <tran.ngoc.vinh@sun-asterisk.com>
2019-11-22 07:30:33 +0700

    add defind swagger for login and logout

--- app/Http/Controllers/Swagger.php
+++ app/Http/Controllers/Swagger.php
@@ -9,4 +9,40 @@
  *         version="1.0.0"
  *     )
  * )
+ * 
+ *  @SWG\Definition(
+ *     definition="InternalServerError",
+ *     description="Internal server error!",
+ *     required={"error_code", "error_description"},
+ *     @SWG\Property(
+ *         property="error_code",
+ *         type="string",
+ *         example="500",
+ *     ),
+ *     @SWG\Property(
+ *         property="error_description",
+ *         type="string",
+ *         example="Internal server error!"
+ *     )
+ * )
+ *
+ *  @SWG\Definition(
+ *     definition="AuthenticationError",
+ *     required={"error_code", "error_description"},
+ *     @SWG\Property(
+ *         property="error_code",
+ *         type="string",
+ *         example="1002",
+ *     ),
+ *     @SWG\Property(
+ *         property="error_description",
+ *         type="string",
+ *         example="認証できませんでした。認証情報をご確認お願いします。"
+ *     )
+ * )
+ * 
+ * @SWG\Tag(
+ *     name="Authorization",
+ *     description="Authorization function"
+ * )
  */
